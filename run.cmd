e12ae0ffb69f4e49c3c9d6611f4778dd5001a873
Tom Eugelink <tbee@tbee.org>
2024-02-24 09:12:34 +0100

    run the boot jar

--- run.cmd
+++ run.cmd
@@ -2,7 +2,7 @@ if exist javaHome.cmd (
     call javaHome.cmd
 )
 
-call %JAVA_HOME%\bin\java -DspotifySlideshow.tecl=C:\Users\tom\prj\spotifySlideshow.tecl -jar target\spotifySlideshow-1.0-SNAPSHOT-boot.jar
+call %JAVA_HOME%\bin\java -Dconfig.tecl=C:\Users\tom\prj\spotifySlideshow.tecl -jar target\spotifySlideshow-1.0-SNAPSHOT-boot.jar
 pause
 
 rem run with: java -jar target/spotifySlideshow-*.jar
