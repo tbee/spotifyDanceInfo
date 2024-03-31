d64d304aff888d2f7c9f22bbab91b46ad26b283e
Tom Eugelink <tbee@tbee.org>
2024-03-31 22:01:07 +0200

    initial connect form

---
+++ web/src/main/java/org/tbee/spotifyDanceInfoWeb/IndexController.java
@@ -0,0 +1,197 @@
+package org.tbee.spotifyDanceInfoWeb;
+
+import jakarta.servlet.http.HttpSession;
+import org.apache.hc.core5.http.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String index(Model model) {
+        ConnectForm connectForm = new ConnectForm();
+        model.addAttribute("ConnectForm", connectForm);
+
+        Cfg cfg = new Cfg();
+        if (cfg.webapiRefreshToken() != null) {
+            connectForm.setClientId(cfg.webapiClientId());
+            connectForm.setClientSecret(cfg.webapiClientSecret());
+            connectForm.setRedirectUrl("https://nyota.softworks.nl/SpotifyDanceInfo.html"); // TBEERNOT generate URL
+            connectForm.setRefreshToken(cfg.webapiRefreshToken());
+        }
+        return "index";
+    }
+
+    @PostMapping
+    public String indexSubmit(HttpSession session, Model model, @ModelAttribute ConnectForm connectForm) {
+        try {
+            SpotifyConnectData spotifyConnectData = new SpotifyConnectData()
+                    .clientId(connectForm.getClientId())
+                    .clientSecret(connectForm.getClientSecret())
+                    .redirectUrl(connectForm.getRedirectUrl())
+                    .refreshToken(connectForm.getRefreshToken().isBlank() ? null : connectForm.getRefreshToken());
+            session.setAttribute("SpotifyConnectData", spotifyConnectData);
+
+            // Spotify API
+            SpotifyApi spotifyApi = spotifyApi(session);
+
+            // Forward to Spotify
+            if (connectForm.getRefreshToken().isBlank()) {
+                URI authorizationCodeUri = spotifyApi.authorizationCodeUri()
+                        .scope("user-read-playback-state,user-read-currently-playing")
+                        .build().execute();
+                return "redirect:" + authorizationCodeUri.toURL().toString();
+            }
+
+            // Get the access token
+            AuthorizationCodeCredentials authorizationCodeCredentials = spotifyApi.authorizationCodeRefresh().build().execute();
+
+            // Update connect data
+            spotifyConnectData
+                    .refreshToken(authorizationCodeCredentials.getRefreshToken() != null ? authorizationCodeCredentials.getRefreshToken() : spotifyConnectData.refreshToken())
+                    .accessToken(authorizationCodeCredentials.getAccessToken());
+
+            return "redirect:/";
+        }
+        catch (IOException | SpotifyWebApiException | ParseException e) {
+            throw new RuntimeException("Problem connecting to Sportify webapi", e);
+        }
+    }
+
+    @GetMapping("/spotifyCallback")
+    public String spotifyCallback(@RequestParam("code") String authorizationCode) {
+        SpotifyApi spotifyApi = spotifyApi(null); // TBEERNOT how to match this to a session?
+        AuthorizationCodeCredentials authorizationCodeCredentials = spotifyApi.authorizationCode(authorizationCode).build().execute();
+
+        SpotifyConnectData spotifyConnectData = spotifyConnectData(null);
+        spotifyConnectData
+                .refreshToken(authorizationCodeCredentials.getRefreshToken() != null ? authorizationCodeCredentials.getRefreshToken() : spotifyConnectData.refreshToken())
+                .accessToken(authorizationCodeCredentials.getAccessToken());
+
+        return "index";
+    }
+
+    private SpotifyApi spotifyApi(HttpSession session) {
+        try {
+            SpotifyConnectData spotifyConnectData = spotifyConnectData(session);
+            return new SpotifyApi.Builder()
+                    .setClientId(spotifyConnectData.clientId())
+                    .setClientSecret(spotifyConnectData.clientSecret())
+                    .setRedirectUri(new URI(spotifyConnectData.redirectUrl()))
+                    .setRefreshToken(spotifyConnectData.refreshToken())
+                    .build();
+        }
+        catch (URISyntaxException e) {
+            throw new RuntimeException("Problem connecting to Sportify webapi", e);
+        }
+    }
+
+    private static SpotifyConnectData spotifyConnectData(HttpSession session) {
+        SpotifyConnectData spotifyConnectData = (SpotifyConnectData) session.getAttribute("SpotifyConnectData");
+        return spotifyConnectData;
+    }
+
+    public static class SpotifyConnectData {
+        private String clientId;
+        private String clientSecret;
+        private String redirectUrl;
+        private String refreshToken;
+        private String accessToken;
+
+        public String clientId() {
+            return clientId;
+        }
+
+        public SpotifyConnectData clientId(String clientId) {
+            this.clientId = clientId;
+            return this;
+        }
+
+        public String clientSecret() {
+            return clientSecret;
+        }
+
+        public SpotifyConnectData clientSecret(String clientSecret) {
+            this.clientSecret = clientSecret;
+            return this;
+        }
+
+        public String redirectUrl() {
+            return redirectUrl;
+        }
+
+        public SpotifyConnectData redirectUrl(String redirectUrl) {
+            this.redirectUrl = redirectUrl;
+            return this;
+        }
+
+        public String refreshToken() {
+            return refreshToken;
+        }
+
+        public SpotifyConnectData refreshToken(String refreshToken) {
+            this.refreshToken = refreshToken;
+            return this;
+        }
+
+        public String accessToken() {
+            return accessToken;
+        }
+
+        public SpotifyConnectData accessToken(String accessToken) {
+            this.accessToken = accessToken;
+            return this;
+        }
+    }
+
+    public static class ConnectForm {
+        private String clientId;
+        private String clientSecret;
+        private String redirectUrl;
+        private String refreshToken;
+
+        public String getClientId() {
+            return clientId;
+        }
+
+        public void setClientId(String clientId) {
+            this.clientId = clientId;
+        }
+
+        public String getClientSecret() {
+            return clientSecret;
+        }
+
+        public void setClientSecret(String clientSecret) {
+            this.clientSecret = clientSecret;
+        }
+
+        public String getRedirectUrl() {
+            return redirectUrl;
+        }
+
+        public void setRedirectUrl(String redirectUrl) {
+            this.redirectUrl = redirectUrl;
+        }
+
+        public String getRefreshToken() {
+            return refreshToken;
+        }
+
+        public void setRefreshToken(String refreshToken) {
+            this.refreshToken = refreshToken;
+        }
+    }
+}
---
+++ web/src/test/java/org/tbee/spotifyDanceInfo/spotifyDanceInfoWeb/SpotifyDanceInfoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tbee.spotifyDanceInfo.spotifyDanceInfoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyDanceInfoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
