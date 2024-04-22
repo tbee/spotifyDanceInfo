82d28a8be5aabe871c0c7b3b8904475efb29d4a5
Tom Eugelink <tbee@tbee.org>
2024-04-22 21:39:29 +0200

    POST request arrives

--- qrks/src/main/java/org/tbee/spotifyDanceInfoQrks/HtmlResource.java
+++ qrks/src/main/java/org/tbee/spotifyDanceInfoQrks/HtmlResource.java
@@ -3,18 +3,25 @@ package org.tbee.spotifyDanceInfoQrks;
 import groovy.util.logging.Log;
 import jakarta.inject.Inject;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
+import org.jboss.resteasy.annotations.providers.multipart.PartType;
 import org.jboss.resteasy.plugins.providers.html.Renderable;
 import org.tbee.spotifyDanceInfo.Cfg;
 
-import java.time.LocalDateTime;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,7 +37,7 @@ public class HtmlResource {
 
     @GET
     @Path("")
-    public Renderable query(@Context HttpServletRequest httpServletRequest, @QueryParam("name") @DefaultValue("Buddy") String name) {
+    public Renderable connect(@Context HttpServletRequest httpServletRequest, @QueryParam("name") @DefaultValue("Buddy") String name) {
 
         ConnectForm connectForm = new ConnectForm();
         connectForm.abbreviations(cfg.getListofDanceAbbreviations());
@@ -46,12 +53,33 @@ public class HtmlResource {
                 .with("ConnectForm", connectForm);
     }
 
-    @GET
-    @Path("{path}") // @Path("{path: .*}")
-    public Renderable path(@PathParam("path") String path) {
-        return thymeleaf.template("index")
-                .with("name", getOr(path, "Dude"))
-                .with("now", LocalDateTime.now());
+
+    @POST
+    @Path("")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response connect(@MultipartForm ConnectForm connectForm) throws URISyntaxException {
+//        LOGGER.log(Level.INFO, "name: {0} ", name);
+//        LOGGER.log(
+//                Level.INFO,
+//                "uploading file: {0},{1},{2},{3}",
+//                new Object[]{
+//                        part.getMediaType(),
+//                        part.getName(),
+//                        part.getFileName(),
+//                        part.getHeaders()
+//                }
+//        );
+//        try {
+//            Files.copy(
+//                    part.getContent(),
+//                    Paths.get(uploadedPath.toString(), part.getFileName().orElse(generateFileName(UUID.randomUUID().toString(), mediaTypeToFileExtension(part.getMediaType())))),
+//                    StandardCopyOption.REPLACE_EXISTING
+//            );
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return Response.ok().build();
+        return Response.temporaryRedirect(new URI("/")).build(); // redirect not working
     }
 
     private String getOr(String name, String elseValue) {
@@ -62,10 +90,22 @@ public class HtmlResource {
     }
 
     public static class ConnectForm {
+
+        @FormParam("clientId")
         private String clientId;
+        @FormParam("clientSecret")
         private String clientSecret;
+        @FormParam("redirectUrl")
         private String redirectUrl;
         private List<Cfg.Abbreviation> abbreviations;
+
+        @FormParam("file")
+        @PartType(MediaType.APPLICATION_OCTET_STREAM)
+        public InputStream file;
+
+        @FormParam("fileName")
+        @PartType(MediaType.TEXT_PLAIN)
+        public String fileName;
 
         public String getClientId() {
             return clientId;
