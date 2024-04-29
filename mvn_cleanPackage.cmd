09100eb80b2e7725d20616bde04fdcb4992dd25d
Tom Eugelink <tbee@tbee.org>
2024-04-29 07:06:34 +0200

    uberjar instead of springboot

--- mvn_cleanPackage.cmd
+++ mvn_cleanPackage.cmd
@@ -2,7 +2,10 @@ if exist javaHome.cmd (
     call javaHome.cmd
 )
 call mvnw.cmd versions:set
-call mvnw.cmd clean package
+call mvnw.cmd clean package 
 pause
+
+rem -Dquarkus.native.additional-build-args="--initialize-at-run-time=org.apache.poi.util.RandomSingleton"  -P native
+rem --trace-object-instantiation=java.security.SecureRandom 
 
 rem run with: java -jar target/spotifyDanceInfo.jar
