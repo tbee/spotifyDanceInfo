f3b499f1f60f5f1b6c38519ba9af85ea7275f412
Tom Eugelink <tbee@tbee.org>
2024-12-22 19:21:19 +0100

    remove quarkus version

--- qrks/mvn_runSpringBootable.cmd
+++
@@ -1,9 +0,0 @@
-if exist ..\javaHome.cmd (
-    call ..\javaHome.cmd
-)
-
-for %%f in (target\spotifydanceinfoqrks-*-boot.jar) do (
-    echo %%f
-	call %JAVA_HOME%\bin\java -Xms256m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError -Dconfig.tecl=..\..\spotifyDanceInfo.tecl -Dloader.path=. -Dloader.home=. -jar %%f
-)
-pause
