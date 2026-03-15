8dc4dffec6b02d418d469fc2ecc6f5ff85b60501
Tom Eugelink <tbee@tbee.org>
2026-03-15 11:43:31 +0100

    use podman plugin

--- web/src/main/container/start.sh
+++
@@ -1,6 +0,0 @@
-# initialize the data directory (--update=none do NOT overwrite an existing file)
-cp --update=none --recursive /data-init /data
-
-# start application
-cd /data
-java -jar /app/*.jar
